7c94a2d2d7afad2ae6a8390290c838ef01fd7e00
murilovargas <murilo.vargas@qitech.com.br>
2024-09-09 23:08:34 -0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, SharedModule],
+  imports: [UserModule, SharedModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
