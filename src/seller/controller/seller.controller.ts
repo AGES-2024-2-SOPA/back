cce00ee8c6e1ebbc26bf12022c7a39672327a547
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2024-09-12 21:48:48 -0300

    Add initial files to add a seller

---
+++ src/seller/controller/seller.controller.ts
@@ -0,0 +1,115 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Delete,
+    Put,
+} from '@nestjs/common';
+
+import { SellerService } from '../service/seller.service';
+import { Seller } from '@prisma/client';
+import { SellerDTO } from '../dtos/seller.dto';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+
+@Controller()
+export class SellerController {
+    constructor(private sellerService: SellerService) {}
+
+    @ApiOperation({ summary: 'Create a new seller' })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                user: {
+                    type: 'object',
+                    properties: {
+                        username: {
+                            type: 'string',
+                            example: 'name',
+                        },
+                        email: {
+                            type: 'string',
+                            example: 'name@gmail.com',
+                        },
+                        password: {
+                            type: 'string',
+                            example: '123',
+                        },
+                        document_number: {
+                            type: 'string',
+                            example: '333',
+                        },
+                        role: {
+                            type: 'object',
+                            properties: {
+                                enumerator: {
+                                    type: 'string',
+                                    example: 'seller',
+                                },
+                            },
+                        },
+                    },
+                },
+                company_name: {
+                    type: 'string',
+                    example: 'ferro_velho',
+                },
+                trading_name: {
+                    type: 'string',
+                    example: 'ferro',
+                },
+                document_number: {
+                    type: 'string',
+                    example: '999',
+                },
+                email: {
+                    type: 'string',
+                    example: 'aaa@gmail.com',
+                },
+                phone_country_code: {
+                    type: 'string',
+                    example: '55',
+                },
+                phone_area_code: {
+                    type: 'string',
+                    example: '51',
+                },
+                phone_number: {
+                    type: 'string',
+                    example: '999999999',
+                },
+                street: {
+                    type: 'string',
+                    example: 'Rua 7',
+                },
+                neighborhood: {
+                    type: 'string',
+                    example: 'AAAAA',
+                },
+                number: {
+                    type: 'string',
+                    example: '444',
+                },
+                city: {
+                    type: 'string',
+                    example: 'Porto Alegre',
+                },
+                complement: {
+                    type: 'string',
+                    example: '4',
+                },
+                postal_code: {
+                    type: 'string',
+                    example: '99999999',
+                },
+            }, 
+        },
+    })
+    @Post()
+    async createSeller(@Body() data: SellerDTO): Promise<Seller> {
+        console.log(data);
+        return this.sellerService.createSeller(data);
+    }
+}
---
+++ src/seller/dtos/seller.dto.ts
@@ -0,0 +1,35 @@
+import { UserDTO } from "src/user/dtos/user.dto";
+
+export class SellerDTO {
+  user: UserDTO;
+  company_name: string;
+  trading_name: string;
+  document_number: string;
+  email: string;
+  phone_country_code: string;
+  phone_area_code: string;
+  phone_number: string;
+  street: string;
+  neighborhood: string;
+  number: string;
+  city: string;
+  complement: string;
+  postal_code: string;
+
+  constructor(seller: SellerDTO) {
+    this.user = seller.user;
+    this.company_name = seller.company_name;
+    this.trading_name = seller.trading_name;
+    this.document_number = seller.document_number;
+    this.email = seller.email;
+    this.phone_country_code = seller.phone_country_code;
+    this.phone_area_code = seller.phone_area_code;
+    this.phone_number = seller.phone_number;
+    this.street = seller.street;
+    this.neighborhood = seller.neighborhood;
+    this.number = seller.number;
+    this.city = seller.city;
+    this.complement = seller.complement;
+    this.postal_code = seller.postal_code;
+  }
+}
